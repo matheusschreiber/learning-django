6f07ba42ac80d868ca477da1c154495ec82d2e5d
Matheus <mt.schreiber01@gmail.com>
2023-07-25 12:54:52 -0300

    :art: integrating new theme on user pages

--- learningdjango/base/views.py
+++ learningdjango/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -57,9 +57,8 @@ def registerPage(request):
       user.save()
       login(request, user)
       return redirect('home')
-
-  else:
-    messages.error(request, 'An error has occurred during registration')
+    else:
+      messages.error(request, 'An error has occurred during registration')
 
   return render(request, 'base/login_register.html', {'form': form})
    
@@ -125,39 +124,82 @@ def userProfile(request, pk):
   context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics':topics}
   return render(request, 'base/profile.html', context)
 
+@login_required(login_url='login')
+def updateUser(request):
+  form = UserForm(instance=request.user)
+  
+  if request.method == "POST":
+    form = UserForm(request.POST, instance=request.user)
+    if form.is_valid():
+      form.save()
+      return redirect('user-profile', pk=request.user.id)
+
+  
+  context={'form': form}
+  return render(request, 'base/update-user.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
+
     if request.method == 'POST':
+      topic_name = request.POST.get('topic')
+      topic, created = Topic.objects.get_or_create(name=topic_name)
+
+      Room.objects.create(
+        host=request.user,
+        topic=topic,
+        name=request.POST.get('name'),
+        description=request.POST.get('description')
+      )
+
+      return redirect('home')
+      
+      
       # here i could do some logic to check if the fields are valid
       
       # or i can use the built in thing
-      form = RoomForm(request.POST)
-      if form.is_valid():
-        room = form.save(commit=False)
-        room.host = request.user
-        room.save()
-        return redirect('home')
+      # form = RoomForm(request.POST)
+      # if form.is_valid():
+      #   room = form.save(commit=False)
+      #   room.host = request.user
+      #   room.save()
+      #   return redirect('home')
 
-    context={'form': form}
+    context={'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
   room = Room.objects.get(id=pk)
+  topics = Topic.objects.all()
+  
   form = RoomForm(instance=room)
 
   if not request.user == room.host:
     return HttpResponse('You are not allowed to update this')
 
   if request.method == "POST":
-    form = RoomForm(request.POST, instance=room)
-    if form.is_valid():
-      form.save()
-      return redirect('home')
+    topic_name = request.POST.get('topic')
+    topic, created = Topic.objects.get_or_create(name=topic_name)
+
+    room.name = request.POST.get('name')
+    room.description = request.POST.get('description')
+    room.topic = topic
+    room.save()
+
+    return redirect('home')
+
+    # form = RoomForm(request.POST, instance=room)
+    # if form.is_valid():
+    #   form.save()
+    #   return redirect('home')
   
-  context = {'form':form}
+  
+  
+  context = {'form':form, 'topics':topics, 'room': room}
   return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
