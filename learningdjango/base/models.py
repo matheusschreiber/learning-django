39bfd2cff4633de7d14d56229de7f57d97bd356f
Matheus <mt.schreiber01@gmail.com>
2023-07-25 10:47:56 -0300

    :sparkles: finish main functionalities

--- learningdjango/base/models.py
+++ learningdjango/base/models.py
@@ -10,20 +10,20 @@ class Topic(models.Model):
       return self.name
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    # participants = \
-    updated = models.DateTimeField(auto_now=True) # every update the timestamps updates
-    created = models.DateTimeField(auto_now_add=True) # only update once
-    
-    class Meta:
-      # ordering = ['updated', 'created'] crescent order
-      ordering = ['-updated', '-created'] # decrescent order
-    
-    def __str__(self):
-      return self.name
+  host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+  name = models.CharField(max_length=200)
+  description = models.TextField(null=True, blank=True)
+  participants = models.ManyToManyField(User, related_name='participants', blank=True)
+  updated = models.DateTimeField(auto_now=True) # every update the timestamps updates
+  created = models.DateTimeField(auto_now_add=True) # only update once
+  
+  class Meta:
+    # ordering = ['updated', 'created'] crescent order
+    ordering = ['-updated', '-created'] # decrescent order
+  
+  def __str__(self):
+    return self.name
     
 class Message(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,6 +31,9 @@ class Message(models.Model):
   body = models.TextField()
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ['-updated', '-created'] # decrescent order
 
   def __str__(self):
     return self.body[0:50]
